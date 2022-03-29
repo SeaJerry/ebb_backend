6efd06284cea8171b5ca61a2701766e801e020fb
Jerry_Sea <Jsea@kindsys.us>
2022-03-29 10:31:19 -0700

    added connection.js and server.js

---
+++ connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+let mongoURL = "";
+
+if (process.env.NODE_ENV === "production") {
+  mongoURL = process.env.DB_URL;
+} else {
+  mongoURL = "mongodb://localhost/ebb_backend"; // match current mongodb for localhost
+}
+
+mongoose.connect(mongoURL);
+
+module.exports = mongoose;
---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(logger('dev'));
+app.use(cors())
+
+app.listen(PORT, () => {
+    console.log(`Listening in on port: ${PORT}`)
+})
