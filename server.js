a9717f56e31eaf52332a7094b3e7fbab8f1423e6
Jerry_Sea <Jsea@kindsys.us>
2022-03-31 19:48:39 -0700

    adding orgin site

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://seajerry.github.io/ebb_client/#/");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  res.setHeader("Access-Control-Allow-Credentials", false);
+  res.setHeader("Access-Control-Allow-Credentials", true);
   // handle OPTIONS method
   if ("OPTIONS" == req.method) {
     return res.sendStatus(200);
