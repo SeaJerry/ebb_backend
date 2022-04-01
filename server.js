992044a91f66503fee8f4c38f7c690946aed80b4
Jerry_Sea <Jsea@kindsys.us>
2022-03-31 19:45:13 -0700

    changed credentials to false

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Credentials", false);
   // handle OPTIONS method
   if ("OPTIONS" == req.method) {
     return res.sendStatus(200);
